859829e02ef28dd5402112717e92e8b9d0400ca6
jeremimedina011 <jeremimedina90@gmail.com>
2023-03-31 12:27:06 -0600

    mi tercer commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:proyecto_currys/pages/login_page.dart';
-
+import 'package:provider/provider.dart';
+import 'package:proyecto_currys/pages/formularioscreens/lenguajes_programacion_screen.dart';
+import 'package:proyecto_currys/pages/home/consumo/login_page.dart';
+import 'package:proyecto_currys/pages/home/paginas_principales/creacion_curriculum.dart';
+import 'package:proyecto_currys/pages/home/paginas_principales/home_page.dart';
+import 'package:proyecto_currys/pages/home/paginas_principales/ver_curriculums.dart';
+import 'package:proyecto_currys/providers/users_providers.dart';
+import 'package:proyecto_currys/widgets/formulario/lenguajes_programacion.dart';
+import 'package:proyecto_currys/widgets/skills_seletion.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,21 +18,30 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // ignore: unnecessary_new
-    return new MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: AnimatedSplashScreen(
-          splash: SizedBox(
-            // ignore: sort_child_properties_last
-            child: Image.asset(
-              'assets/images/login_splash.png',
-              fit: BoxFit.cover,
-            ),
-            height: 170,
-            width: 160,
-          ),
-          duration: 3100,
-          splashTransition: SplashTransition.fadeTransition,
-          nextScreen: const LoginPage()),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<UserProviders>(
+          create: (context) => UserProviders()..getCurriculums(),
+        )
+      ],
+      builder: (context, _) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: AnimatedSplashScreen(
+              splash: SizedBox(
+                // ignore: sort_child_properties_last
+                child: Image.asset(
+                  'assets/images/login_splash.png',
+                  fit: BoxFit.cover,
+                ),
+                height: 170,
+                width: 160,
+              ),
+              duration: 3100,
+              splashTransition: SplashTransition.fadeTransition,
+              nextScreen: LoginPage()),
+        );
+      },
     );
   }
 }
--- lib/pages/splahs_App/splash.dart
+++ lib/pages/splahs_App/splash.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:proyecto_currys/pages/home.dart';
-import 'package:proyecto_currys/pages/login_page.dart';
+import 'package:proyecto_currys/pages/home/paginas_principales/perfiles.dart';
+import 'package:proyecto_currys/pages/home/consumo/login_page.dart';
 
+// ignore: camel_case_types
 class splash extends StatefulWidget {
   const splash({super.key});
 
@@ -16,24 +17,21 @@ class _splashState extends State<splash> {
     super.initState();
     _navigateToHome();
   }
-
   _navigateToHome() async {
     await Future.delayed(const Duration(milliseconds: 1900), () {});
     // ignore: use_build_context_synchronously
     Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-          builder: (context) => const LoginPage(),
+    context,
+    MaterialPageRoute(
+      builder: (context) => const  LoginPage(),
         ));
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          
         ),
       ),
     );
